4e311fbe27e36c0d03029f70360563c3a74037ef
bharathboienipalli <bharathboienipalli@gmail.com>
2025-05-21 10:33:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-let firstName = document.getElementsByName("fname");
-let lastName = document.getElementsByName("lname");
 function getFormvalue() {
+const firstName = document.querySelector("#fname").value.trim();
+const lastName = document.querySelector("#lname").value.trim();
     alert(firstName + " "+lastName)
-
 }
